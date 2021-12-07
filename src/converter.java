69d52ab283e0b6776c6596b98cd923050e003b31
Manuel Notter <notter.manuel@gmail.com>
2021-12-07 21:17:49 +0100

    Initial Commit

---
+++ src/converter.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+import java.util.random.*;
+
+
+
+
+public class converter {
+
+
+public static void main(String[] args) {
+
+    // Read user input
+    Scanner scan = new Scanner(System.in);
+    System.out.println("Please enter a number from 0 to 8 and press ENTER....");
+    int userIn = scan.nextInt();
+
+    // Increment
+    userIn++;
+
+
+
+
+
+    }
+}
